58981ac161f179d9f2fd19568181fbfb1f75ee53
Natasha Seelam <nseelam1@gmail.com>
2020-06-27 20:47:14 -0400

    Model with q-learning implemented.

--- model.py
+++ model.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+2020.06.27
+
+The following setup trains an RL agent
+and observes the moves the agent takes
+via a visualization (GameGrid).
+"""
 
 #Numerics
 import numpy as np
@@ -32,42 +39,72 @@ random.seed(314159)
 np.random.seed(123)
 
 # Create the environment
-
 env = gym.make('gym_2048:game2048-v0', N_episodes=params.N_episodes)
 env.seed(456)
 env.action_space.seed(789)
 
-# Set-up the q-learning
+# Set-up learning algorithm
 qlearn = QLearn(actions=range(env.action_space.n), 
                 alpha=params.Alpha, 
                 gamma=params.Gamma, 
                 epsilon=params.Epsilon)
 
-env.reset()
-frames = {0: {"state": env._get_observation(), 
+# Begin training
+# --------------------- #
+
+all_frames = []
+for Ntrain in range(5):
+    print("Training episode", Ntrain + 1)
+    #epoch = 0
+    env.reset()
+    frames = {0: {"state": env._get_observation(), 
               "reward": 0, 
               "action": 
               "start", "game_over": False}}
-epochs = 1
-done = False
-while not done:
-    action = env.action_space.sample()
-    state, reward, done = env.step(action)
-    # Put each rendered frame into dict for animation
-    frames.update({epochs: {
-        'state': state,
+    #done = False
+    #while not done:
+    for epoch in range(0, 1000):
+        print("Ep:", Ntrain+1, "epoch:", epoch+1)
+        currState = env._get_observation()
+        action = qlearn.chooseAction(currState)
+        nextState, reward, done = env.step(action)
+        qlearn.learn(currState, action, reward, nextState)
+        frames.update({epoch: {
+        'state': nextState,
         'action': params.action_dict[action],
         'reward': reward,
-        'game_over': env.Game.game_over,
-        }
-    })
+        'game_over': env.Game.game_over}
+        })
+        #epoch += 1
+    all_frames.append(frames)
 
-    epochs += 1
+# --------------------- #
 
-traj = {idx: frames[idx] for idx in range(100)}
+#env.reset()
+#frames = {0: {"state": env._get_observation(), 
+#              "reward": 0, 
+#              "action": 
+#              "start", "game_over": False}}
+#epochs = 1
+#done = False
+#while not done:
+#    action = env.action_space.sample()
+#    state, reward, done = env.step(action)
+#    # Put each rendered frame into dict for animation
+#    frames.update({epochs: {
+#        'state': state,
+#        'action': params.action_dict[action],
+#        'reward': reward,
+#        'game_over': env.Game.game_over,
+#        }
+#    })
+#
+#    epochs += 1
+#
+#traj = {idx: frames[idx] for idx in range(100)}
 
-g = GameGrid(frames[0]['state'], traj, 1.5)
-g.mainloop()
+#g = GameGrid(frames[0]['state'], traj, 1.5)
+#g.mainloop()
 
 #for epoch in range(1):
 #    observation = env.reset()
