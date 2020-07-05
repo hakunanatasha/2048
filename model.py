8aa48508f7afdaa717829eb208188661a1172882
hakunanatasha <nseelam1@gmail.com>
2020-07-05 16:27:50 -0400

    Changed counter to save every N games; also changed saving file names to keep track of params.

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ import queue
 #import gzip
 import pickle as pkl
 
-def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None):
+def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None, tmpsave=500):
     """
     Training regime for models
     that train for a fixed episodic/epoch level
@@ -54,7 +54,9 @@ def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None):
     score = 0
 
     if savetmp is not None:
-        outputtxt = open(savetmp + "scores.txt", "w")
+        scorefile = "_".join(["scores", str(params.Alpha), str(params.Gamma), str(params.Epsilon)])
+        outputtxt = open(savetmp + scorefile + ".csv", "w")
+        outputtxt.writelines(["Episode\tMove\tScore\tGameStatus(W/L)\t\n"])
 
     for eps in range(params.N_episodes):
         print("Training episode = ", eps + 1, "/", params.N_episodes)
@@ -71,11 +73,12 @@ def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None):
         epoch = 0
 
         while not done:
-            line = ["Ep:", eps+1, "epoch:", epoch+1, "Score", score, "Game status:", env.Game.game_over, "\n"]
+            line = [eps+1, epoch+1, score, env.Game.game_over, "\n"]
             outputtxt.writelines("\t".join([str(i) for i in line]))
 
             currState = env._get_observation()
             action = qlearn.chooseAction(currState)
+
             # Updated state, new board score, reached 2048?, reward
             nextState, reward, done, score = env.step(action)
             qlearn.learn(currState, action, reward, nextState)
@@ -89,16 +92,18 @@ def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None):
             })
 
             # Iterate the counters
-            countr += 1
             epoch += 1
 
-            if savetmp is not None and countr >= 100:
+            if savetmp is not None and countr >= tmpsave:
+                print("Saving temporary file.")
                 countr = 0
 
-                with open(savetmp + "qlearn_tmp.pkl", "wb") as f:
+                qtmp = "_".join(["qlearn_tmp", str(params.Alpha), str(params.Gamma), str(params.Epsilon)])
+                ftmp = "_".join(["states_tmp", str(params.Alpha), str(params.Gamma), str(params.Epsilon)])
+                with open(savetmp + qtmp + ".pkl", "wb") as f:
                     pkl.dump(qlearn, f)
 
-                with open(savetmp + "states_tmp.pkl", "wb") as f:
+                with open(savetmp + ftmp + ".pkl", "wb") as f:
                     pkl.dump(all_frames, f)
 
             if epoch >= maxepoch:
@@ -106,29 +111,33 @@ def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None):
 
 
         all_frames.append(frames)
+        countr += 1
         outputtxt.writelines("\n\n")
 
     print("All episodes completed")
 
-    qname = "qlearn_eps" + str(params.N_episodes) + ".pkl"
-    sname = "states_eps" + str(params.N_episodes) + ".pkl"
+    qname = "_".join(["qlearn", str(params.N_episodes), str(params.Alpha), str(params.Gamma), str(params.Epsilon)])
+    sname = "_".join(["states", str(params.N_episodes), str(params.Alpha), str(params.Gamma), str(params.Epsilon)])
     
-    with open(savetmp + qname, "wb") as f:
+    with open(savetmp + qname + ".pkl", "wb") as f:
         pkl.dump(qlearn, f)
 
-    with open(savetmp + sname, "wb") as f:
+    with open(savetmp + sname + ".pkl", "wb") as f:
         pkl.dump(all_frames, f)
 
     outputtxt.close()
 # --------------------- #
 
 if __name__ == "__main__":
+    
+    print("Learning Rate:", params.Alpha, " Reward scaling:", params.Gamma, " Exploration:", params.Epsilon)
 
     #Save directory
     savedir = "data/"
 
     # Reload last qlearn, if None, restart.
-    qlearnfile = "/home/natasha/proj/2048/data/train_1000/qlearn_eps1000.pkl"
+    qlearnfile = None
+    #qlearnfile = "/home/natasha/proj/2048/data/train_1000/qlearn_eps1000.pkl"
 
     # Initialize a random seed for reproducibility
     random.seed(314159)
