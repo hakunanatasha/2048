4955a693b76e7c94465450b6295ed6c1d39fe9b5
hakunanatasha <nseelam1@gmail.com>
2020-07-05 13:25:57 -0400

    Model can take old policy and update it or start fresh

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 """
+2020.07.05
+Reload trained q to add more training iterations
+if on dkm desktop, use hash -r
+
 2020.07.03
 I'll initialize 2 types of training wherein
 (1) Train for a fixed period of epoch moves 
@@ -35,7 +39,7 @@ import queue
 #import gzip
 import pickle as pkl
 
-def run_model(env, qlearn, maxepoch=1e4, savetmp=None):
+def run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=None):
     """
     Training regime for models
     that train for a fixed episodic/epoch level
@@ -46,7 +50,6 @@ def run_model(env, qlearn, maxepoch=1e4, savetmp=None):
     maxepoch-stop training an episode after N (if None, trains until game condition met)
     savetmp - periodically save temp files. 
     """
-    all_frames = []
     countr = 0
     score = 0
 
@@ -54,12 +57,13 @@ def run_model(env, qlearn, maxepoch=1e4, savetmp=None):
         outputtxt = open(savetmp + "scores.txt", "w")
 
     for eps in range(params.N_episodes):
-        print("Training episode = ", eps + 1)
+        print("Training episode = ", eps + 1, "/", params.N_episodes)
 
         env.reset()
 
         frames = {0: {"state": env._get_observation(), 
               "reward": 0, 
+              "total_score": 0,
               "action": 
               "start", "game_over": False}}
 
@@ -80,6 +84,7 @@ def run_model(env, qlearn, maxepoch=1e4, savetmp=None):
             'state': nextState,
             'action': params.action_dict[action],
             'reward': reward,
+            'total_score': score,
             'game_over': env.Game.game_over}
             })
 
@@ -122,6 +127,9 @@ if __name__ == "__main__":
     #Save directory
     savedir = "data/"
 
+    # Reload last qlearn, if None, restart.
+    qlearnfile = "/home/natasha/proj/2048/data/train_1000/qlearn_eps1000.pkl"
+
     # Initialize a random seed for reproducibility
     random.seed(314159)
     np.random.seed(123)
@@ -132,12 +140,28 @@ if __name__ == "__main__":
     env.action_space.seed(789)
 
     # Set-up learning algorithm
-    qlearn = QLearn(actions=range(env.action_space.n), 
-                    alpha=params.Alpha, 
-                    gamma=params.Gamma, 
-                    epsilon=params.Epsilon)
+    if qlearnfile is None:
+        print("Learning with naive prior")
+        qlearn = QLearn(actions=range(env.action_space.n), 
+                        alpha=params.Alpha, 
+                        gamma=params.Gamma, 
+                        epsilon=params.Epsilon)
 
-    run_model(env, qlearn, maxepoch=1e4, savetmp=savedir)
+        all_frames = []
+    else:
+        print("Learning from old file=", qlearnfile)
+        #Load the previous policy
+        with open(qlearnfile, 'rb') as f:
+            qlearn = pkl.load(f)
+
+        #Load the previous history
+        framefile = qlearnfile.split('qlearn')
+        framefile = "".join([framefile[0], 'states', framefile[1]])
+        with open(framefile, 'rb') as f:
+            all_frames = pkl.load(f)
+
+
+    run_model(env, qlearn, all_frames, maxepoch=1e4, savetmp=savedir)
 
     #g = GameGrid(frames[0]['state'], traj, 1.5)
     #g.mainloop()
